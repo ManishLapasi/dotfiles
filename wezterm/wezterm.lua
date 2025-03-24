32d44112a04a00af77bcef1297c740a7a76d1c65
mlp7 <mlp7@gatech.edu>
2025-03-24 14:34:33 -0400

    wezterm updates

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -23,6 +23,16 @@ local host_os = get_os()
 config.colors = require("cyberdream")
 config.force_reverse_video_cursor = true
 
+-- Font configuration
+config.font_dirs = { 'C:\\Users\\manis\\AppData\\Local\\fonts' }
+config.font = wezterm.font_with_fallback({
+    {
+        family = "CaskaydiaCove Nerd Font",
+        weight = "Regular",
+    }
+})
+config.font_size = 10
+
 -- Window Configuration
 config.initial_rows = 35
 config.initial_cols = 180
@@ -39,9 +49,10 @@ config.cursor_blink_rate = 250
 
 -- Tab Bar Configuration
 config.enable_tab_bar = true
-config.hide_tab_bar_if_only_one_tab = false
+config.hide_tab_bar_if_only_one_tab = true
 config.show_tab_index_in_tab_bar = false
 config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = false
 config.colors.tab_bar = {
     background = config.window_background_image and "rgba(0, 0, 0, 0)" or transparent_bg,
     new_tab = { fg_color = config.colors.background, bg_color = config.colors.brights[6] },
@@ -73,9 +84,53 @@ wezterm.on("format-tab-title", function(tab, _, _, _, hover)
     }
 end)
 
+-- CmdLine
+config.status_update_interval = 1000
+wezterm.on("update-status", function(window, pane)
+	local basename = function(s)
+		return string.gsub(s, "(.*[/\\])(.*)", "%2")
+	end
+
+	-- Current working directory
+	local cwd = pane:get_current_working_dir()
+	if cwd then
+		if type(cwd) == "userdata" then
+			cwd = basename(cwd.file_path)
+		else
+			cwd = basename(cwd)
+		end
+	else
+		cwd = ""
+	end
+
+	-- Current command
+	local cmd = pane:get_foreground_process_name()
+	cmd = cmd and basename(cmd) or ""
+
+	-- Time
+	local time = wezterm.strftime("%H:%M")
+
+	-- Right status
+	window:set_right_status(wezterm.format({
+		{ Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
+		{ Text = " | " },
+		{ Foreground = { Color = "#e0af68" } },
+		{ Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
+		"ResetAttributes",
+		{ Text = " | " },
+		{ Text = wezterm.nerdfonts.md_clock .. "  " .. time },
+		{ Text = "  " },
+	}))
+end)
+
+
 -- Keybindings
 config.keys = {
     { key = "v", mods = "CTRL", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+    { key = "F11", action = wezterm.action.ToggleFullScreen },
+    { key = "r", mods = "CTRL", action = wezterm.action.ReloadConfiguration },
+    { key = "1", mods = "ALT", action = wezterm.action.ActivateTabRelative(-1) },
+    { key = "2", mods = "ALT", action = wezterm.action.ActivateTabRelative(1) },
 }
 
 -- OS-Specific Overrides
